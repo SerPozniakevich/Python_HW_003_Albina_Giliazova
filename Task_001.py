f3fbb449d762721ccb6d38ba60df82936fa02a3f
Sergey Pozniakevich <ven07@yandex.ru>
2022-10-05 00:14:41 +0700

    add solution task 001 teacher

--- Task_001.py
+++ Task_001.py
@@ -2,11 +2,21 @@
 #   Пример:
 # [2, 3, 5, 9, 3] -> на нечётных позициях элементы 3 и 9, ответ: 12
 
+# Var my
+
+# line_n = input('Введите ряд чисел через пробел: ').split()
+# summ_neg_ind = 0
+
+# for i in range(len(line_n)):
+#     if i % 2 != 0:
+#         summ_neg_ind += int(line_n[i])
+# print(f'{line_n} -> cумма элементов на нечётных позициях равна: {summ_neg_ind}')
+
+# Var tutor
+
 line_n = input('Введите ряд чисел через пробел: ').split()
 summ_neg_ind = 0
 
-for i in range(len(line_n)):
-    if i % 2 != 0:
-        summ_neg_ind += int(line_n[i])
+for i in range(1, len(line_n), 2):
+    summ_neg_ind += int(line_n[i])
 print(f'{line_n} -> cумма эелментов на нечётных позициях равна: {summ_neg_ind}')
-
